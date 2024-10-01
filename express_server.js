373896f7f56e5d476665f7ac466ff90099768e18
BronwenM <bronwen.marchant@gmail.com>
2024-10-01 16:11:33 -0700

    Added update operation

--- express_server.js
+++ express_server.js
@@ -6,6 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 
 //generates a random string to act as key in URL database
+//this could be done with math.random and a different base encode, but this allows for both upper and lower case letters to be used, as well as the option to use symbols, if added to the ALPHA_NUMS string
 function generateRandomString() {
     //define the alpha-numeric chars that are allowed
     const ALPHA_NUMS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -28,19 +29,18 @@ function generateRandomString() {
     return idString;
 }
 
-
 const urlDatabase = {
     b2xVn2: "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
-    res.send("Hello!");
+    res.redirect("/urls");
 });
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-});
+// app.get("/urls.json", (req, res) => {
+//     res.json(urlDatabase);
+// });
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -51,6 +51,7 @@ app.get("/urls", (req, res) => {
     res.render("urls_index", templateVars);
 });
 
+//CREATE OPERATION
 app.post('/urls', (req, res) => {
 
     const newID = generateRandomString();
@@ -65,6 +66,7 @@ app.post('/urls', (req, res) => {
     res.redirect(`/urls/${newID}`);
 })
 
+//redirect user to link associated with the id
 app.get('/u/:id', (req, res) => {
     if(Object.hasOwnProperty(req.params.id)){ 
         res.redirect(urlDatabase[req.params.id]);
@@ -74,15 +76,18 @@ app.get('/u/:id', (req, res) => {
     }
 })
 
+//access the 'create urls' page
 app.get('/urls/new', (req, res) => {
     res.render('urls_new');
 });
 
+//Get individual URLs by ID, this is not the same as accessing a short link, which redirects to the appropriate site
 app.get("/urls/:id", (req, res) => {
     const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
     res.render("urls_show", templateVars);
 });
 
+//DELETE OPERATION
 app.post('/urls/:id/delete', (req, res) => {
     const linkID = req.params.id;
     console.log(linkID);
@@ -91,6 +96,25 @@ app.post('/urls/:id/delete', (req, res) => {
     console.log(urlDatabase);
 
     res.redirect('/urls');
+})
+
+//UPDATE OPERATION
+app.post('/urls/:id/update', (req, res) => {
+    const newLink = req.body.newURL;
+    const linkID = req.params.id;
+
+    console.log(req.params, req.body);
+    
+    if(!req.body.newURL){
+        console.warn("No link was entered! Try again");
+        res.redirect(`/urls/${linkID}`);
+    } else {
+        urlDatabase[linkID] = newLink;
+    
+        console.log(urlDatabase);
+        
+        res.redirect("/urls");
+    }
 })
 
 app.listen(PORT, () => {
